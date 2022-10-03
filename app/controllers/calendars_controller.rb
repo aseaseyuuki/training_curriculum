cdc6ae36e5ed4fecdbefcda1010edd2d382d7887
aseaseyuuki <114049905+aseaseyuuki@users.noreply.github.com>
2022-10-03 20:20:25 +0900

    Issue4~6の解消　曜日()訂正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -19,7 +19,7 @@ class CalendarsController < ApplicationController
   end
 
   def get_week
-    wdays = ['日','月','火','水','木','金','土']
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     @week_days = []
     @todays_date = Date.today
